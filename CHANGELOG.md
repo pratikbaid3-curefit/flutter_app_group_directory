e8b26421f357e648a05b54ccb2e07699731e1ba3
Dimitri Dessus <dimitri.dessus@gmail.com>
2024-10-11 14:34:33 +0200

    ✨ Add Android platform

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+* ✨ Add Android platform.
+
 ## 1.0.0
 
 * 🎉 Initial release.
---
+++ example/android/app/src/main/kotlin/com/example/flutter_app_group_directory_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_app_group_directory_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
